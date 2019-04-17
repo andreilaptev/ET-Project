f0213e30da05568851ee2d942e3b8e5db9cf39f2
Andrei <bokken2008@gmail.com>
2019-04-17 10:27:16 -0400

    Nurse login implemented

--- src/app/nurse-login/nurse-login.component.ts
+++ src/app/nurse-login/nurse-login.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Observable } from 'rxjs';
+import { ActivatedRoute, Router } from "@angular/router";
+import { concat } from 'rxjs/internal/observable/concat';
+import { HttpClient } from '@angular/common/http';
+import { Nurse } from '../nurse';
+import { debug } from 'util';
+import { LocalStorageService, SessionStorageService } from 'ngx-webstorage';
 
 @Component({
   selector: 'app-nurse-login',
@@ -7,9 +15,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NurseLoginComponent implements OnInit {
 
-  constructor() { }
+  requestedNurse: any;
+  authorizationError: boolean = false;
+  email: string;
+  password: string;
+
+  constructor(private data: DataService, private router: Router, private sessionStorage: SessionStorageService) { }
 
   ngOnInit() {
   }
+
+  onLogin(){
+
+    this.requestLogin();    
+  }
+
+
+ 
+  checkPass(){
+    console.log(this.requestedNurse);
+     
+  }
+
+    requestLogin(){
+      this.data.patientLogin(this.email, this.password).subscribe(
+        patient => {
+          this.requestedNurse = patient      
+          console.log(this.requestedNurse);
+
+          debugger
+
+          if (this.requestedNurse === null) {
+            this.router.navigate(['nurse-login']);
+            this.authorizationError = true;
+          }else            
+              if (this.requestedNurse.password === this.password) {
+
+                this.sessionStorage.store('nurse', this.requestedNurse);
+                this.router.navigate(['nurse_page']);
+              }
+              else {
+                this.authorizationError = true;
+              }
+
+        
+        })  
+  
+    }
 
 }
