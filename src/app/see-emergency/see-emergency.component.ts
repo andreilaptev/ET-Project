4d564fe72f3b13c83fd468bd0f189bf524c62d62
Andrei <bokken2008@gmail.com>
2019-04-17 11:21:23 -0400

    Emergencies component updated

--- src/app/see-emergency/see-emergency.component.ts
+++ src/app/see-emergency/see-emergency.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Observable } from 'rxjs';
+import { ActivatedRoute, Router } from "@angular/router";
+import { concat } from 'rxjs/internal/observable/concat';
+import { HttpClient } from '@angular/common/http';
+import { Nurse } from '../nurse';
+import { debug } from 'util';
+import { LocalStorageService, SessionStorageService } from 'ngx-webstorage';
 
 @Component({
   selector: 'app-see-emergency',
@@ -7,9 +15,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SeeEmergencyComponent implements OnInit {
 
-  constructor() { }
+  emergencies: any;
+ /* emergency = {
+
+    "title": "Terrbile headache",
+    "body": "Can't feel my body anymore",
+    "patient_id": "5cb60129b60e05270b27a2d0",
+    "createdAt": "2019-04-16T20:27:52.628Z",
+    "updatedAt": "2019-04-16T20:27:52.628Z"
+
+  };*/
+
+  constructor(private data: DataService, private router: Router, private sessionStorage: SessionStorageService) { }
 
   ngOnInit() {
+
+    this.data.seeEmergencyList().subscribe(
+      data => this.emergencies = data
+
+    )
   }
 
 }
